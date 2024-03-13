f60a16cba6834a2ab1a5e8a8d9ba17d6affbb591
Roger <rogeriskrenov@gmail.com>
2024-03-13 12:08:50 +0100

    commit 4

--- boobies.py
+++ boobies.py
@@ -3,18 +3,20 @@ print('I am touching them')
 msg="Separatists vs Han Solo"
 print(msg)
 
-#combining strings
+#combines strings
 first_name = 'Albert'
 second_name = 'Sabathon'
 full_name = first_name + ' ' + second_name
 print(full_name)
 
-#lists and accessing them
+#makes a list and gives acces to them
 bikes = ['super' , 'duper' , 'superlisios']
 first_bike = bikes[0]
+last_bike = bikes[-1] 
+
 print(first_bike)
-last_bike = bikes[-1]
 print(last_bike)
 
+#prints out the numbers until it reaches 5
 for i in range(5):
     print(i)
