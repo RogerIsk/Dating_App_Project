2e1a864596d51504056fc3e4db5d43e66da0912b
Roger <rogeriskrenov@gmail.com>
2024-03-13 11:39:37 +0100

    adding a new preject

---
+++ boobies.py
@@ -0,0 +1,18 @@
+print('I am touching them')
+
+msg="Separatists vs Han Solo"
+print(msg)
+
+#combining strings
+first_name = 'Albert'
+second_name = 'Sabathon'
+full_name = first_name + ' ' + second_name
+print(full_name)
+
+#lists and accessing them
+bikes = ['super' , 'duper' , 'superlisios']
+first_bike = bikes[0]
+print(first_bike)
+last_bike = bikes[-1]
+print(last_bike)
+
