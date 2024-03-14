c18a0d92ba8cfd119b38045e782092b4cf3a0885
RogerIsk <rogeriskrenov@gmail.com>
2024-03-14 18:10:47 +0100

    Commit number 2

--- DCI Class Dating App.py
+++ DCI Class Dating App.py
@@ -1,24 +1,36 @@
+#a list of people looking for love
+people = [['Markus' , 'Anton' , 'Benjamin' , 'Erik' , 'Burak', 'Dess' , 'Roger' , 'Roman' , 'Sunny' , 'Hassan'] ,
+          ['Hiba' , 'Morgane','Vanessa']] 
 
-#a list of subjects looking for love / 'dimensions' is a list that contains information that cannot be changed
-dimension = [['Markus' , 'Anton' , 'Benjamin' , 'Erik' , 'Burak', 'Dess' , 'Roger' , 'Roman' , 'Sunny' , 'Hassan'] ,
-             ['Hiba' , 'Morgane','Vanessa']] 
+male   = ['man','male','men','males', 1]           #we give the variable different values through the usage of a list
+female = ['woman','female','women','females', 2]   #we give the variable different values through the usage of a list
+player = []
 
-male = 1
-female = 2
-
-print('Choose your sex:')
-
-row1 = 0 #we give the variable 'row1' the value of 0 / this is the first row of the list
-row2 = 1 #we give the variable 'row2' the value of 1 / this is the second row of the list
-
-for i in range(len(dimension[row1])): #for each number in the range of the length of the list
-    print(dimension[row1][i]) #print out the name in the list
+print('\n''Viable options in this Demo are "Male" and "Female".''\n''\n''Choose your sex:''\n')
+print('1. Male              ''2. Female               ')
 
 
+if player == '1''man''male''men''males':
+    player= input('Enter your choice: ')
+    player == 'man'
+    
+print('\n''You have chosen to be a', player, '\n')
+
+print('\n''Choose what you want to date:''\n')
+print('1. Males              ''2. Females               ''3. Both''\n')
+male = 1 #we give the variable 'male' the value of 1 / this is the first row of the list
+female = 2 #we give the variable 'female' the value of 2 / this is the second row of the list
+
+#we print out all the male names in the list
+#for i in range(len(people[male])): #for each name in the first row
+ #   print(people[male][i]) #print out a name from the list
 
 
-#for sublist in dimension: #for each sublist in the list / there are a total of 2 sublists
+
+
+#for sublist in people: #for each sublist in the list / there are a total of 2 sublists
  #   for name in sublist: #for each name in the list
   #      if name == 'Hassan': #if the name is Morgane
    #         print('Choose the lover you want to be') #prints out the name of the person you want to be
     #        break  #breaks the loop
+
